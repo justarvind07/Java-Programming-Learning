0ed31bfb5fa21adbc52dc6a83d2bc44dea26f00a
Arvind Singh <justarvind07@gmail.com>
2025-06-01 18:11:05 +0530

    Create Unique_Element.java

---
+++ src/Target_Sum/Unique_Element.java
@@ -0,0 +1,34 @@
+package Target_Sum;
+
+import java.util.Scanner;
+
+public class Unique_Element {
+    static int uniqueElement(int[] arr){
+        int ans = 0;
+        for (int i =0; i< arr.length; i++){
+            for (int j = i+1; j<arr.length; j++){
+                if (arr[i] == arr[j]){
+                    arr[i] =-1;
+                    arr[j] =-1;
+                }
+            }
+        }
+        for (int i = 0; i< arr.length; i++){
+            if (arr[i]> 0){
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter "+ n+ " Array Input");
+        for (int i = 0; i< arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(uniqueElement(arr));
+    }
+}
