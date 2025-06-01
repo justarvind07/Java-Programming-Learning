2b7aef3d4117accbeadeb4812edee554373636d2
Arvind Singh <justarvind07@gmail.com>
2025-06-01 18:15:46 +0530

    Update Unique_Element.java

--- src/Target_Sum/Unique_Element.java
+++ src/Target_Sum/Unique_Element.java
@@ -8,7 +8,7 @@ public class Unique_Element {
         for (int i =0; i< arr.length; i++){
             for (int j = i+1; j<arr.length; j++){
                 if (arr[i] == arr[j]){
-                    arr[i] =-1;
+                    arr[i] =-1; // if number is matching then that value will be -1
                     arr[j] =-1;
                 }
             }
