c3e0eee7205d2df25e9854a3a7c49458322f7141
Arvind Singh <justarvind07@gmail.com>
2025-06-02 14:07:45 +0530

    Create SecondSmallestElement.java

---
+++ src/Target_Sum/SecondSmallestElement.java
@@ -0,0 +1,31 @@
+package Target_Sum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecondSmallestElement {
+    static int secondSmallestElement(int [] arr){
+        if (arr.length<2) return -1;
+        Arrays.sort(arr);
+        int ans = 0;
+        for (int i = 0; i< arr.length; i++){
+            if (arr[i] != arr[0]){
+                ans = arr[i];
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array size : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter : "+n+" Element in the array : ");
+        for (int i = 0; i< arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("This is second smallest Element in the array : " + secondSmallestElement(arr));
+    }
+}
