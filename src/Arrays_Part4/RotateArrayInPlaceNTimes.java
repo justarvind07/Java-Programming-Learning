b3ae31fc4e8948023205161f98773fd94768a1c3
Arvind Singh <justarvind07@gmail.com>
2025-06-17 10:30:29 +0530

    Update RotateArrayInPlaceNTimes.java

--- src/Arrays_Part4/RotateArrayInPlaceNTimes.java
+++ src/Arrays_Part4/RotateArrayInPlaceNTimes.java
@@ -10,30 +10,42 @@ public class RotateArrayInPlaceNTimes {
         System.out.println();
     }
 
-    static int[] nTimeArrayRotation(int[] arr, int n) {
-        int m = arr.length;
-        n = n % m;
-        for (int i = m-n; i<m;i++){
-            int [] temp= new int[m];
-            temp[i++] = arr[i];
+    static void reverseArray(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-        return arr;
+    }
+
+    static void kTimeArrayRotation(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        if (k == 0) return; // no rotation is needed
+        reverseArray(arr, 0, n - 1); // Reverse complete arrays
+        reverseArray(arr, 0, k - 1); // reverse array first k element
+        reverseArray(arr, k, n - 1); // reverse the remaining element
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Array Size: ");
         int n = sc.nextInt();
-        int[]arr = new int[n];
-        System.out.print("Enter "+n+ " Element in Array: ");
-        for (int i =0; i< arr.length; i++){
+        int[] arr = new int[n];
+        System.out.print("Enter " + n + " Element in Array: ");
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.print("Enter k: ");
         int k = sc.nextInt();
         System.out.print("Before Rotation : ");
         printArrays(arr);
-        int [] ans = nTimeArrayRotation(arr, n);
-        printArrays(ans);
+        //int[] ans = kTimeArrayRotation(arr, n);
+        kTimeArrayRotation(arr, k);
+        System.out.print("After rotation: ");
+        printArrays(arr);
+        sc.close();
     }
 }
