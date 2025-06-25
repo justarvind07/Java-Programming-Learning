5acf8cea336af3b2b709830ae7d0bb2868f5060c
Arvind Singh <justarvind07@gmail.com>
2025-06-25 20:47:24 +0530

    new

---
+++ src/TwoPointers/Non_Decreasing_Square_Arrays.java
@@ -0,0 +1,40 @@
+package TwoPointers;
+
+import java.util.Scanner;
+
+public class Non_Decreasing_Square_Arrays {
+    static void printArrays(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static int[] non_Decreasing_Square_Arrays(int[] arr) {
+        int n = arr.length;
+        int[] sqr = new int[n];
+        int i = 0;
+        while (i < n) {
+            sqr[i] = arr[i] * arr[i];
+            i++;
+        }
+        return sqr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int x = sc.nextInt();
+        int[] arr = new int[x];
+        System.out.print("Enter " + x + " Element in array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Before Square each numbers: ");
+        printArrays(arr);
+        int[] sqr = non_Decreasing_Square_Arrays(arr);
+        System.out.print("After Square each numbers: ");
+        System.out.println();
+        printArrays(sqr);
+    }
+}
